5120676a3e71f2038ea1e43a0b28a8ac2210794e
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-11-02 17:16:33 -0300

    fix: terminando homepage-paciente #84

--- src/front/assets/scripts/home-paciente.js
+++ src/front/assets/scripts/home-paciente.js
@@ -1,9 +1,33 @@
-const buttonModal = document.getElementById("btn-open-modal");
+const buttons = document.querySelectorAll("button");
 const modal = document.getElementById("modal");
 const btnCloseModal = document.getElementById("btn-close-modal");
+const htmlConteudo = document.querySelector("html");
+const bodyConteudo = document.querySelector("body");
 
-buttonModal.addEventListener("click", function () {
 
-    modal.showModal();
+for (const button of buttons) {
+    button.addEventListener("click", function () {
+    
+        modal.showModal();
+        bloquearRolagem();
+    
+    })
 
-})
+}
+
+btnCloseModal.addEventListener("click", function () {
+
+    modal.close();
+    permitirRolagem();
+
+});
+
+const bloquearRolagem = () => {
+    htmlConteudo.classList.add("rolagem-bloqueada");
+    bodyConteudo.classList.add("rolagem-bloqueada");
+}
+
+const permitirRolagem = () => {
+    htmlConteudo.classList.add("rolagem-permitida");
+    bodyConteudo.classList.add("rolagem-permitida");
+}
