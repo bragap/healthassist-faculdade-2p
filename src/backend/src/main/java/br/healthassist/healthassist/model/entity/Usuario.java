110175a6895d7760f95a515fc343281149984567
Jhonata S. Dias <118060258+jhonstudentx@users.noreply.github.com>
2023-11-03 06:42:19 -0300

    Implementando as rotas do Usuario #38

--- src/backend/src/main/java/br/healthassist/healthassist/model/entity/Usuario.java
+++ src/backend/src/main/java/br/healthassist/healthassist/model/entity/Usuario.java
@@ -1,4 +1,35 @@
 package br.healthassist.healthassist.model.entity;
 
+import br.healthassist.healthassist.model.enums.StatusAutorizacao;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "apelido")
+    private String apelido;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @Column(name = "autorizacao")
+    @Enumerated(EnumType.STRING)
+    private StatusAutorizacao autorizacao;
 }
--- src/backend/src/main/java/br/healthassist/healthassist/service/UsuarioService.java
+++ src/backend/src/main/java/br/healthassist/healthassist/service/UsuarioService.java
@@ -1,4 +1,12 @@
 package br.healthassist.healthassist.service;
 
-public class UsuarioService {
+import br.healthassist.healthassist.model.entity.Usuario;
+
+import java.util.Optional;
+
+public interface UsuarioService {
+
+    Usuario salvarUsuario(Usuario usuario);
+
+    Optional<Usuario> obterPorId(Long id);
 }
