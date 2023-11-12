063ba4a2a0325e685f9b9bd9813fc7709ab871a2
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-11-12 09:53:25 -0300

    code: iniciando integração de front com back #43

--- src/front/assets/scripts/home-paciente.js
+++ src/front/assets/scripts/home-paciente.js
@@ -1,9 +1,9 @@
+// html
 const buttons = document.querySelectorAll("button");
 const modal = document.getElementById("modal");
 const btnCloseModal = document.getElementById("btn-close-modal");
 const htmlConteudo = document.querySelector("html");
 const bodyConteudo = document.querySelector("body");
-
 
 for (const button of buttons) {
     button.addEventListener("click", function () {
@@ -31,3 +31,31 @@ const permitirRolagem = () => {
     htmlConteudo.classList.add("rolagem-permitida");
     bodyConteudo.classList.add("rolagem-permitida");
 }
+
+
+
+// Requisição com axios	
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+axios.get(url)
+     .then( response => {
+        const dados = response.data;
+
+        const cardDoctor = document.getElementById("cards-section");
+
+        dados.forEach(user => {
+            let listDoctors = ""
+            listDoctors += `
+            <div id="card-individual">
+            <h5>${user.name}</h5>
+            <p> ${user.username}</p>
+            <button type="button" id="btn-open-modal">Agende uma consulta!</button>
+            </div>
+            `;
+            cardDoctor.innerHTML += listDoctors; 
+        })
+     })
+     .catch(error => {
+        console.error('Ocorreu um erro:', error);
+      });
