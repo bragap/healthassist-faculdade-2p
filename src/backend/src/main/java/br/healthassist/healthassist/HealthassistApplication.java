866a4a00e969daff723fd1266476c8fda2bc7340
Jhonata S. Dias <118060258+jhonstudentx@users.noreply.github.com>
2023-11-13 19:07:16 -0300

    Update HealthassistApplication.java

--- src/backend/src/main/java/br/healthassist/healthassist/HealthassistApplication.java
+++ src/backend/src/main/java/br/healthassist/healthassist/HealthassistApplication.java
@@ -2,9 +2,14 @@ package br.healthassist.healthassist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class HealthassistApplication {
+
+	public void corsMappings(CorsRegistry registry){
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthassistApplication.class, args);
