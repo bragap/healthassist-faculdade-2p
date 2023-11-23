55a8f268591736d30b57401636f4750ad2a75f97
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-11-23 16:31:26 -0300

    feat: frontend dos indicadores #50

--- src/front/assets/scripts/indicadores.js
+++ src/front/assets/scripts/indicadores.js
@@ -1,9 +1,21 @@
 // elements
-const painel = document.getElementById('card-layout');
-let listConsultas = "";
+const painel1 = document.getElementById('ind-1');
+const painel2 = document.getElementById('ind-2');
+const painel3 = document.getElementById('ind-3');
+const painel4 = document.getElementById('ind-4');
+const painel5 = document.getElementById('ind-5');
+let listConsultas1 = "";
+let listConsultas2 = "";
+let listConsultas3 = "";
+let listConsultas4 = "";
+let listConsultas5 = "";
 
 // endpoints
-const url = ""
+const urlQtdePacientes = "http://localhost:8080/indicadores/quantidade-pacientes";
+const urlMediaConsultasMedico = "http://localhost:8080/indicadores/media-consultas-medico"
+const url3=""
+const url4=""
+const url5=""
 
 
 // FUNÇÕES
@@ -16,67 +28,133 @@ const tipoUsuario = localStorage.getItem('tipoUsuario');
 // Função para verificar a autorização do usuário
 function checkAuthorization() {
 
-    if (tipoUsuario === "MEDICO" || tipoUsuario === "PACIENTE") {
-        redirectTo('error.html');
-    }
+  if (tipoUsuario === "MEDICO" || tipoUsuario === "PACIENTE") {
+    redirectTo('error.html');
+  }
 }
 
 checkAuthorization();
 
+// meses do ano
+const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro", "Janeiro"];
 
 
-// get de todas os cancelamentos de consultas
-axios.get(url)
-  .then(response => {
-    const dados = response.data;
+// Ind 1 - Pacientes cadastrados em cada um dos últimos 12 meses
+document.addEventListener('DOMContentLoaded', function () {
+  axios.get(urlQtdePacientes)
+    .then(response => {
+      const dados = response.data;
 
-    if(dados === null){
-
-      painel.innerHTML = "<p>Nenhum cancelamento de consulta.</p>";
-      return;
-
-    } else {
-      dados.forEach((consulta) => {
-  
-        listConsultas += `
+      dados.forEach((qtdePacientes, index) => {
+        listConsultas1 += `
     <div id="card-individual">
       <div id="card-conteudo">
-          <div id=card-title> Consulta ${consulta.id} </div>
+          <div id=card-title> Mês: ${meses[index]} 
+           </div>
               <div id="box-esquerda">
-                  <p id="nome-usuario">Nome do paciente: ${consulta.name}</p>
-                  <p>Médico: ${consulta.phone}</p>
-                  <p>Horario: 11:00</p>
-                  <p>Dia da semana: Segunda-feira</p>
+                  <p id="nome-usuario">Quantidade de pacientes cadastrados: ${qtdePacientes}</p>
               </div>
-              <div id="box-direita">
-              <button type="button" class="aceitar">Aprovar cancelamento</button>
-            </div>
-          
-      </div>
+        </div>
     </div>
-  
           `
+      })
+      painel1.innerHTML = listConsultas1;
     })
-    painel.innerHTML = listConsultas;
-  }
+    .catch(error => console.error(error))
+})
+
+// Ind - 2 : Taxa Média de Consultas Mensais por Médico
+document.addEventListener('DOMContentLoaded', function () {
+  axios.get(urlMediaConsultasMedico)
+    .then(response => {
+      const dados = response.data;
+
+      dados.forEach((qtdePacientes, index) => {
+        listConsultas2 += `
+    <div id="card-individual">
+      <div id="card-conteudo">
+          <div id=card-title> Mês: ${meses[index]} 
+           </div>
+              <div id="box-esquerda">
+                  <p id="nome-usuario">Quantidade de pacientes cadastrados: ${qtdePacientes}</p>
+              </div>
+        </div>
+    </div>
+          `
+      })
+      painel2.innerHTML = listConsultas2;
     })
+    .catch(error => console.error(error))
+})
 
+// Ind - 3: Número de Médicos Cadastrados na Clínica
+document.addEventListener('DOMContentLoaded', function () {
+  axios.get(url3)
+    .then(response => {
+      const dados = response.data;
 
-// PUT será inserido
+      dados.forEach((qtdePacientes, index) => {
+        listConsultas3 += `
+    <div id="card-individual">
+      <div id="card-conteudo">
+          <div id=card-title> Mês: ${meses[index]} 
+           </div>
+              <div id="box-esquerda">
+                  <p id="nome-usuario">Quantidade de pacientes cadastrados: ${qtdePacientes}</p>
+              </div>
+        </div>
+    </div>
+          `
+      })
+      painel3.innerHTML = listConsultas3;
+    })
+    .catch(error => console.error(error))
+})
 
-// exibiçao tela de loading
-function showLoading() {
+// Ind - 4: Taxa Média de Consultas por Cliente
+document.addEventListener('DOMContentLoaded', function () {
+  axios.get(url4)
+    .then(response => {
+      const dados = response.data;
 
-  document.getElementById('loading').style.display = 'flex';
+      dados.forEach((qtdePacientes, index) => {
+        listConsultas4 += `
+    <div id="card-individual">
+      <div id="card-conteudo">
+          <div id=card-title> Mês: ${meses[index]} 
+           </div>
+              <div id="box-esquerda">
+                  <p id="nome-usuario">Quantidade de pacientes cadastrados: ${qtdePacientes}</p>
+              </div>
+        </div>
+    </div>
+          `
+      })
+      painel4.innerHTML = listConsultas4;
+    })
+    .catch(error => console.error(error))
+})
 
-  setTimeout(function () {
-    document.getElementById('loading').style.display = 'none';
+// Ind - 5: Taxa Média de Consultas Mensais por Médico
+document.addEventListener('DOMContentLoaded', function () {
+  axios.get(url5)
+    .then(response => {
+      const dados = response.data;
 
-  }, 2000);
-}
-function redirectTo(destination) {
-  showLoading();
-  setTimeout(function () {
-    window.location.href = destination;
-  }, 2000);
-}
+      dados.forEach((qtdePacientes, index) => {
+        listConsultas5 += `
+    <div id="card-individual">
+      <div id="card-conteudo">
+          <div id=card-title> Mês: ${meses[index]} 
+           </div>
+              <div id="box-esquerda">
+                  <p id="nome-usuario">Quantidade de pacientes cadastrados: ${qtdePacientes}</p>
+              </div>
+        </div>
+    </div>
+          `
+      })
+      painel5.innerHTML = listConsultas5;
+    })
+    .catch(error => console.error(error))
+})
