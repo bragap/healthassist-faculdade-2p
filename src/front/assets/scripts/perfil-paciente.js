4bacc4880648895ee0e58efcaedba5ecb0eae287
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-11-30 17:57:37 -0300

    fix: design site

--- src/front/assets/scripts/perfil-paciente.js
+++ src/front/assets/scripts/perfil-paciente.js
@@ -32,6 +32,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // dados do paciente
     let str = "";
     str += `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="#0367A6" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+          </svg>
     <h3>Suas informações pessoais</h3>
               <p>Nome completo: <span>${dados.nomeCompleto}</span></p>
               <p>Usuário:  <span>${dados.usuario.apelido}</span></p>
